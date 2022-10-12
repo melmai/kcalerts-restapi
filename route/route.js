46e57ed1cf6fce8b8c016888145a9c580628eace
Mel <mel@mel.codes>
2022-10-12 14:20:11 -0700

    refactor code to extract getRouteIDs() fxn

--- route/route.js
+++ route/route.js
@@ -1,26 +1,17 @@
 window.addEventListener("DOMContentLoaded", generateAlerts);
 
 async function generateAlerts() {
-  const path = "/sitecore/content/KCGov/home/depts/metro/schedules-maps/007";
+  const path = "/sitecore/content/KCGov/home/depts/metro/schedules-maps/545";
   //   const path = window.location.pathname;
   const alertContainer = document.getElementById("advisory-block-wrapper");
   const BASE_URL = "http://107.23.133.228:8090/developer/api/v2";
   const API_KEY = "4oJedLBt80WP-d7E6Ekf5w";
 
   // TODO replace static route with parsed version from window.location.pathname
-  const routeName = "7"; // route 7
+  const routeName = parseRoutes(path) || "7"; // route 7
 
-  // get all routes and find the one we're looking for
-  // TODO find for all routes (multi-route pages)
-  const routes = await fetch(`${BASE_URL}/routes?api_key=${API_KEY}`).then(
-    (res) => res.json()
-  );
-
-  // find the route ID we're looking for
-  const route = routes.mode[1].route.find(
-    (route) => route.route_name === routeName
-  );
-  const routeID = route.route_id;
+  const routeID = await getRouteIDs(BASE_URL, API_KEY, routeName);
+  console.log(routeID);
 
   // find alerts based on route ID
   const alerts = await fetch(
@@ -29,4 +20,22 @@ async function generateAlerts() {
   console.log(alerts.alerts);
 }
 
-function parseRoutes(path) {}
+function parseRoutes(path) {
+  return path.match(/(\d+)/)[0].replaceAll("0", "");
+}
+
+async function getRouteIDs(baseURL, apiKey, routeNames) {
+  // get all routes and find the one we're looking for
+  // TODO find for all routes (multi-route pages)
+  console.log(routeNames);
+  const routes = await fetch(`${baseURL}/routes?api_key=${apiKey}`).then(
+    (res) => res.json()
+  );
+
+  // find the route ID we're looking for
+  const route = routes.mode[1].route.find(
+    (route) => route.route_name === routeNames
+  );
+  console.log(route);
+  return route.route_id;
+}
