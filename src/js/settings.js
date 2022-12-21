79ef22a0bc75dd2c4137da218a9b9a55ab9692db
Mel <mel@mel.codes>
2022-12-21 12:55:46 -0800

    updates API domain and key

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,8 +1,15 @@
 // general settings
 const IS_REMOTE = false;
 
+// Example API calls
+// TEST
+// https://kcm-api.ibi-test.com/developer/api/v2/alerts?api_key=gvMjFrABizrQwye9KBD3KB&format=json
+
+// PROD
+// https://kcm-api.ibi-transit.com/developer/api/v2/alerts?api_key=3QxRRLWBsUAZbfT62GEB2Q&format=json
+
 // API credentials
-const BASE_URL = "https://kcm-api-test.ibi-transit.com/developer/api/v2";
-const API_KEY = "gvMjFrABizrQwye9KBD3KB";
+const BASE_URL = "https://kcm-api-transit.ibi-transit.com/developer/api/v2";
+const API_KEY = "3QxRRLWBsUAZbfT62GEB2Q";
 
 export { IS_REMOTE, BASE_URL, API_KEY };
