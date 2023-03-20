b5da4f3e9fd8a1f2bb00b2af38717867af97e0b1
Mel <mel@mel.codes>
2023-03-20 11:10:32 -0700

    fixes banner load method

--- src/js/home-alert-banner.js
+++ src/js/home-alert-banner.js
@@ -12,11 +12,18 @@
 import "bootstrap";
 import { IS_REMOTE, LOCAL_ALERTS_DATA, REMOTE_ALERTS_API } from "./settings";
 
-window.addEventListener("DOMContentLoaded", systemAlertInit);
+if (document.readyState !== "loading") {
+  systemAlertInit();
+} else {
+  window.addEventListener("DOMContentLoaded", systemAlertInit);
+}
 
 function systemAlertInit() {
-  const systemAlertContainer = document.getElementById("system-alerts-banner");
+  // const systemAlertContainer = document.querySelector(
+  //   ".alert-advanced > .container"
+  // );
 
+  const systemAlertContainer = document.getElementById("collapseAlertText");
   // set fetch type
   const ALERT_URL = IS_REMOTE ? REMOTE_ALERTS_API : LOCAL_ALERTS_DATA;
 
@@ -24,9 +31,42 @@ function systemAlertInit() {
     .then((res) => res.json())
     .then((res) => {
       const bannerAlerts = getBannerAlerts(res.alerts);
-      let featuredAlert = bannerAlerts[0];
+
+      // if (bannerAlerts.length === 0) return; // don't proceed if no system alerts
+
+      // let featuredAlert = bannerAlerts[0];
+      let featuredAlert = {
+        alert_id: 1713,
+        effect_name: "Snow Routes",
+        effect: "DETOUR",
+        cause_name: "snow",
+        cause: "WEATHER",
+        header_text:
+          "Snow is falling across King County, but Metro buses are currently able to operate on their regular routes. Transit Alerts will be sent out if there is a need to switch to snow routes.",
+        short_header_text:
+          "Snow is falling across King County, but Metro buses are regular operation",
+        url: "https://kingcountymetro.blog/",
+        severity: "Significant",
+        created_dt: "1670339549",
+        last_modified_dt: "1670428019",
+        service_effect_text:
+          "Bus, Marine and Rail service is operating on snow routes.",
+        alert_lifecycle: "New",
+        banner_text:
+          "Snow is falling across King County, but Metro buses are currently able to operate on their regular routes. Transit Alerts will be sent out if there is a need to switch to snow routes.",
+        effect_periods: [{ effect_start: "1670339520", effect_end: "" }],
+        affected_services: {
+          services: [
+            { route_type: "0", mode_name: "Light Rail" },
+            { route_type: "3", mode_name: "Bus" },
+            { route_type: "4", mode_name: "Boat" },
+          ],
+          elevators: [],
+        },
+      };
+
       if (bannerAlerts.length > 1) featuredAlert = featuredAlert(bannerAlerts);
-      systemAlertContainer.append(buildBanner(featuredAlert));
+      systemAlertContainer.prepend(buildBanner(featuredAlert));
     });
 }
 
@@ -53,20 +93,20 @@ function featuredAlert(alerts) {
 // build banner with most recent alert
 function buildBanner(alert) {
   const bannerContent = document.createElement("div");
-  bannerContent.setAttribute(
-    "class",
-    "alert alert-warning alert-dismissible fade show"
-  );
-  bannerContent.setAttribute("role", "alert");
+  // bannerContent.setAttribute(
+  //   "class",
+  //   "alert alert-warning alert-dismissible fade show"
+  // );
+  // bannerContent.setAttribute("role", "alert");
 
-  const alertInner = document.createElement("div");
-  alertInner.setAttribute("class", "container");
+  // const alertInner = document.createElement("div");
+  // alertInner.setAttribute("class", "container");
 
-  const row = document.createElement("div");
-  row.setAttribute("class", "row");
+  // const row = document.createElement("div");
+  // row.setAttribute("class", "row");
 
-  const col11 = document.createElement("div");
-  col11.setAttribute("class", "col-11");
+  // const col11 = document.createElement("div");
+  // col11.setAttribute("class", "col-11");
 
   // title
   const title = document.createElement("h2");
@@ -82,27 +122,28 @@ function buildBanner(alert) {
   bodyText.textContent = alert.banner_text;
 
   // close bttn
-  const buttonContainer = document.createElement("div");
-  buttonContainer.setAttribute("class", "col");
+  // const buttonContainer = document.createElement("div");
+  // buttonContainer.setAttribute("class", "col");
 
-  const closeButton = document.createElement("button");
-  closeButton.setAttribute("type", "button");
-  closeButton.setAttribute("class", "close");
-  closeButton.setAttribute("data-dismiss", "alert");
-  closeButton.setAttribute("aria-label", "Close");
+  // const closeButton = document.createElement("button");
+  // closeButton.setAttribute("type", "button");
+  // closeButton.setAttribute("class", "close");
+  // closeButton.setAttribute("data-dismiss", "alert");
+  // closeButton.setAttribute("aria-label", "Close");
 
-  const spanX = document.createElement("span");
-  spanX.setAttribute("aria-hidden", "true");
-  spanX.textContent = "x";
+  // const spanX = document.createElement("span");
+  // spanX.setAttribute("aria-hidden", "true");
+  // spanX.textContent = "x";
 
-  closeButton.append(spanX);
-  buttonContainer.append(closeButton);
+  // closeButton.append(spanX);
+  // buttonContainer.append(closeButton);
 
   content.append(bodyText);
-  col11.append(title, content);
-  row.append(col11, buttonContainer);
-  alertInner.append(row);
-  bannerContent.append(alertInner);
+  // col11.append(title, content);
+  // row.append(col11, buttonContainer);
+  // alertInner.append(row);
+  // bannerContent.append(alertInner);
+  bannerContent.append(title, content);
 
   return bannerContent;
 }
