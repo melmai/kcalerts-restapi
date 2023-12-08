d09f6e92b7e9c2b92634a9324c2963ecaf2f22c2
Mel <mel@mel.codes>
2023-12-08 13:11:27 -0800

    removes postcss from dev.config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -27,33 +27,17 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-      {
         test: /\.(scss)$/,
         use: [
-          {
-            loader: "style-loader", // inject CSS to page
-          },
-          {
-            loader: "css-loader", // translates CSS into CommonJS modules
-          },
-          {
-            loader: "postcss-loader", // Run post css actions
-            options: {
-              plugins: function () {
-                // post css plugins, can be exported to postcss.config.js
-                return [require("precss"), require("autoprefixer")];
-              },
-            },
-          },
+          // {
+          //   loader: "postcss-loader", // Run post css actions
+          //   options: {
+          //     plugins: function () {
+          //       // post css plugins, can be exported to postcss.config.js
+          //       return [require("precss"), require("autoprefixer")];
+          //     },
+          //   },
+          // },
           {
             loader: "sass-loader", // compiles Sass to CSS
           },
